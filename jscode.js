1e59d1fa839957bb9e183bcbe13ffe97079334e7
Mariusz Borek <mariuszborek242@gmail.com>
2020-09-30 20:53:13 +0200

    fill text areas with scores after computing resistance

--- jscode.js
+++ jscode.js
@@ -12,8 +12,18 @@ function computeResistance() {
     var score1 = Number.parseFloat(thickness1) / Number.parseFloat(lambda1);
     var score2 = Number.parseFloat(thickness2) / Number.parseFloat(lambda2);
 
-    console.log(score1);
-    console.log(score2);
+    // console.log(score1);
+    // console.log(score2);
+
+    var placeToPrintResistace1 = document.getElementById('resistance-1');
+    console.log(placeToPrintResistace1);
+    placeToPrintResistace1.value = score1;
+
+    var placeToPrintResistace2 = document.getElementById('resistance-2');
+    console.log(placeToPrintResistace2);
+    placeToPrintResistace2.value = score2;
+
+
 }
 
 var computeResistanceButton = document.getElementById('compute-resistance');
@@ -26,12 +36,14 @@ function computeHTC() {
     var inputRse = document.getElementById('rse').value;
     var inputR1 = document.getElementById('resistance-1').value;
     var inputR2 = document.getElementById('resistance-2').value;
-    var score = Number.parseInt(inputRsi) +
-        Number.parseInt(inputRse) +
-        Number.parseInt(inputR1) +
-        Number.parseInt(inputR2);
-    var placeToPrint = document.getElementById('htc-score');
-    placeToPrint.innerHTML = 'HTC = ' + score;
+
+    var score = 1 / (Number.parseFloat(inputRsi) +
+        Number.parseFloat(inputRse) +
+        Number.parseFloat(inputR1) +
+        Number.parseFloat(inputR2));
+
+    var placeToPrintHtc = document.getElementById('htc-score');
+    placeToPrintHtc.innerHTML = 'HTC = ' + score;
     console.log('HTC', score);
 }
 
