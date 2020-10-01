dcc2f318fde72a019f15157dd131502336cb563f
Mariusz Borek <mariuszborek242@gmail.com>
2020-10-01 23:41:59 +0200

    added possibility to show and hide rsi/rse table, graphical interface improvement

--- jscode.js
+++ jscode.js
@@ -1,18 +1,13 @@
-function start() {
-    addRow();
-    document.getElementById('table-rsi-rse').style.display = 'none';
-}
-
-window.onload = start();
-
 var actualRowNumber = 0;
 
-function rowNumber() {
+function rowNumberAdd() {
     actualRowNumber = actualRowNumber + 1;
-    console.log(actualRowNumber);
 
 }
 
+function rowNumberSubtract() {
+    actualRowNumber = actualRowNumber - 1;
+}
 
 function addRow() {
     var rowBox = document.getElementById('row-box');
@@ -20,26 +15,35 @@ function addRow() {
     rowBox.appendChild(div);
     div.id = 'input-values-' + actualRowNumber;
     div.innerHTML = '<input type="text" id="material' + actualRowNumber + '" placeholder="input material" /><input type="number" id="thickness-' + actualRowNumber + '" placeholder="input thickness" /><input type="number" id="lambda-' + actualRowNumber + '" placeholder="input lambda-' + actualRowNumber + '" /><input type="number" id="resistance-' + actualRowNumber + '" class="resistance" />';
-    rowNumber();
+    rowNumberAdd();
 }
 
 var rowNumberButton = document.getElementById('row-numbers');
 rowNumberButton.addEventListener('click', addRow);
 
+function subtractRow() {
+    if (actualRowNumber == 1) {
+        return
+    }
+
+    var rowBox = document.getElementById('row-box');
+    var lastDiv = document.getElementById('input-values-' + (actualRowNumber - 1));
+    rowBox.removeChild(lastDiv);
+    rowNumberSubtract()
+}
+
+var rowNumberButton = document.getElementById('subtract-row');
+rowNumberButton.addEventListener('click', subtractRow);
+
 // -----------
 
 function computeResistance() {
-
     for (i = 0; i < actualRowNumber; i++) {
 
         var thickness = document.getElementById('thickness-' + i + '').value;
         var lambda = document.getElementById('lambda-' + i + '').value;
 
-        console.log(thickness);
-        console.log(lambda);
-
         var placeToPrintResistace = document.getElementById('resistance-' + i + '');
-        console.log(placeToPrintResistace);
         placeToPrintResistace.value = Number.parseFloat(thickness) / Number.parseFloat(lambda);
     }
 }
@@ -49,7 +53,7 @@ computeResistanceButton.addEventListener('click', computeResistance);
 
 // -----------
 
-function computeHTC() {
+function computeUvalue() {
     var rsi = document.getElementById('rsi').value;
     var rse = document.getElementById('rse').value;
     var resistanceSum = 0;
@@ -61,12 +65,17 @@ function computeHTC() {
 
     var uValue = 1 / (Number.parseFloat(rsi) + Number.parseFloat(rse) + Number.parseFloat(resistanceSum));
 
-    var placeToPrintHtc = document.getElementById('htc-score');
-    placeToPrintHtc.innerHTML = 'U value = ' + uValue;
+    var placeToPrintHtc = document.getElementById('display-info');
+    if (isNaN(uValue)) {
+        placeToPrintHtc.innerHTML = 'fill all required fields or subtract rows';
+    } else {
+        placeToPrintHtc.innerHTML = 'U value = ' + uValue;
+    }
+
 }
 
 var computeHtcButton = document.getElementById('compute-hrc');
-computeHtcButton.addEventListener('click', computeHTC);
+computeHtcButton.addEventListener('click', computeUvalue);
 
 function showTable() {
     var table = document.getElementById('table-rsi-rse');
@@ -79,3 +88,10 @@ function showTable() {
 
 var showRsiRseButton = document.getElementById('show-rsi-rse-table');
 showRsiRseButton.addEventListener('click', showTable);
+
+function start() {
+    addRow();
+    document.getElementById('table-rsi-rse').style.display = 'none';
+}
+
+window.onload = start();
